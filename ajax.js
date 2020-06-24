107832539c68bf80b1d63979c6595f1b0f3c71da
sandeb <sandrine.debatty@gmail.com>
2020-06-24 08:37:44 +0200

    done

--- ajax.js
+++ ajax.js
@@ -1,12 +1,21 @@
-function fetchAdditionalHTML() {
-    fetch('additional-content.html')
-      .then(function(response) {
-        return response.text();
-      })
-      .then(function(htmlContent) {
-        console.log('received data:', htmlContent);
-        document.querySelector('#additional-content').innerHTML = htmlContent;
+function fetchSimpsonJSON() {
+  const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+  axios.get(url)
+    .then(function(response) {
+      return response.data[0]; // response.data instead of response.json() with fetch
+    })
+    .then(function(simpson) {
+      console.log('data decoded from JSON:', simpson);
+
+      // Build a block of HTML
+      const simpsonHtml = `
+        <p><strong>${simpson.quote}</strong></p>
+        <p>${simpson.character}</p>
+        <img src="${simpson.image}"/>
+      `;
+      document.querySelector('#simpson').innerHTML = simpsonHtml;
       });
   }
   
-  setTimeout(fetchAdditionalHTML, 3000);
+  fetchSimpsonJSON();
+
